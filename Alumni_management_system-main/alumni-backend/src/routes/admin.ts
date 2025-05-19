91b4dbc075523304f2156a2374ec80c58c68877d
Deepanshu Mishra <152648199+Deepanshumishra2004@users.noreply.github.com>
2025-05-19 05:58:14 +0530

    Delete Alumni_management_system-main directory

--- Alumni_management_system-main/alumni-backend/src/routes/admin.ts
+++
@@ -1,9 +0,0 @@
-import express, { RequestHandler } from 'express';
-import { signupAdmin, loginAdmin } from '../controllers/adminController';
-
-const router = express.Router();
-
-router.post('/signup', signupAdmin as RequestHandler);
-router.post('/login', loginAdmin as RequestHandler);
-
-export default router;
